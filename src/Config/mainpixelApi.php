68683f8468a6a7918bdff8028dcd92cabf59fc5e
Jasper Berkhout <jasper@mainpixel.nl>
2016-07-24 00:33:24 +0200

    Added documentation inside config file

--- src/Config/mainpixelApi.php
+++ src/Config/mainpixelApi.php
@@ -1,34 +1,10 @@
 <?php
-/**
- * Mainpixel B.V.  - All Rights Reserved.
- * Written by Jasper Berkhout <jasper@mainpixel.io>.
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
 return [
-
 		/*
-		 * Mainpixel.io API URL.
-		 * Default: https://account.mainpixel.io/api/v1
+		 * Mainpixel.io
 		 *
-		 * If you are running private server or you are a whitelabel/reseller
-		 * you can replace it with your own domain.
+		 * Create your own credentials inside our platform.
 		 *
 		 * Need support? Set a issue in GitHub or send us an e-mail (support@mainpixel.io).
 		 *
@@ -36,7 +12,19 @@ return [
 		 * Please check documentation for a how-to.
 		 */
 
-		'url' => 'http://account.mainpixel.io/api/v1/',
-		'token' => 'aa_572b33a5c41f31c66c8b4579____',
+		'url' => 'YOUR-DOMAIN/api/v1/',
+		'token' => 'YOUR-TOKEN',
 
+		/*
+		 *
+		 * ONLY ENABLED FOR SERVER ADMIN's
+		 *
+		 * If you are running a private server and building a application witch allow people to
+		 * manage their profiles. You can add a API token witch allow 'all' profiles by default.
+		 *
+		 * You can overwrite below value by doing 'config(["MainpixelApi"=>'YOUR-PROFILE-ID'])'
+		 * So when force every api-call with the right selected profileID.
+		 */
+
+		'profile' => '',
 	];

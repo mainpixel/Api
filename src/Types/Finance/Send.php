af02af3c5712fa059a5fc445f2a5aec67b8185cc
Mr. J. del Luna <jasper@mainpixel.nl>
2016-09-08 09:55:33 +0200

    Added send method for sending invoices.
    Send invoices directly or on separate date.

---
+++ src/Types/Finance/Send.php
@@ -0,0 +1,21 @@
+<?php
+/*******************************************************************************
+ * Copyright (C) 2016 - 8-9-16 09:52
+ * Mainpixel B.V.  - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Written by Jasper Berkhout <jasper@mainpixel.io>.
+ ******************************************************************************/
+
+namespace Mainpixel\Api\Types\Finance;
+
+use Mainpixel\Api\MainpixelApi;
+
+class Send extends MainpixelApi{
+
+	protected $path = 'finance/send';
+
+	protected $allowed = [
+		'add',
+	];
+
+}

af97a94edb3ede683d0185e859e1b24059effa9f
Mr. J. del Luna <paperclamp@users.noreply.github.com>
2017-01-12 17:05:02 +0100

    Create InvoiceProducts.php

---
+++ src/Types/Finance/InvoiceProducts.php
@@ -0,0 +1,25 @@
+<?php
+/*******************************************************************************
+ * Copyright (C) 2016 - 1-8-16 13:59
+ * Mainpixel B.V.  - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Written by Jasper Berkhout <jasper@mainpixel.io>.
+ ******************************************************************************/
+
+namespace Mainpixel\Api\Types\Finance;
+
+use Mainpixel\Api\MainpixelApi;
+
+class InvoiceProducts extends MainpixelApi{
+
+	protected $path = 'finance/invoice-products';
+
+	protected $allowed = [
+		'add',
+		'edit',
+		'show',
+		'list',
+		'remove',
+	];
+
+}

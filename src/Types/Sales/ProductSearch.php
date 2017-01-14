e72a694578107bf6f2f08ba4839313706add7392
Mr. J. del Luna <paperclamp@users.noreply.github.com>
2017-01-14 11:04:36 +0100

    Update ProductSearch.php

--- src/Types/Sales/ProductSearch.php
+++ src/Types/Sales/ProductSearch.php
@@ -1,6 +1,6 @@
 <?php
 /*******************************************************************************
- * Copyright (C) 2016 - 10-8-16 16:39
+ * Copyright (C) 2016 - 14-1-17 11:01
  * Mainpixel B.V.  - All Rights Reserved.
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  * Written by Jasper Berkhout <jasper@mainpixel.io>.

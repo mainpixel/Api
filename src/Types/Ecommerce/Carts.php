5a1f3f34c71754e6e6205f12d4f71d2d05edbc0f
Mr. J. del Luna <paperclamp@users.noreply.github.com>
2017-10-17 13:39:15 +0200

    Create Carts.php

---
+++ src/Types/Ecommerce/Carts.php
@@ -0,0 +1,19 @@
+<?php
+/*******************************************************************************
+ * Copyright (C) 2016 - 1-8-16 13:59
+ * Mainpixel B.V.  - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Written by Jasper Berkhout <jasper@mainpixel.io>.
+ ******************************************************************************/
+namespace Mainpixel\Api\Types\Ecommerce;
+use Mainpixel\Api\MainpixelApi;
+class Carts extends MainpixelApi{
+	protected $path = 'ecommerce/carts';
+	protected $allowed = [
+		'add',
+		'edit',
+		'show',
+		'list',
+		'remove',
+	];
+}
